a0e1356db4a70a5d9a731bc6581fe5ed691c0874
Jae Kim <9028364@gmail.com>
2022-06-26 23:00:46 +0900

    [drawing-board] 이미지 다운로드 기능 추가

--- intermediate/drawing-board/src/js/index.js
+++ intermediate/drawing-board/src/js/index.js
@@ -5,6 +5,21 @@ class DrawingBoard {
   backgroundColor = "#ffffff";
   isNavigatorVisible = false;
   undoArray = [];
+  containerEl;
+  canvaseEl;
+  toolbarEl;
+  brushEl;
+  colorPickerEl;
+  brushPanelEl;
+  brushSliderEl;
+  brushSizePreviewEl;
+  eraserEl;
+  navigatorEl;
+  navigatorImageContainerEl;
+  navigatorImageEl;
+  undoEl;
+  clearEl;
+  downloadLinkEl;
 
   constructor() {
     this.assignElement();
@@ -30,6 +45,7 @@ class DrawingBoard {
       this.navigatorImageContainerEl.querySelector("#canvasImg");
     this.undoEl = this.toolbarEl.querySelector("#undo");
     this.clearEl = this.toolbarEl.querySelector("#clear");
+    this.downloadLinkEl = this.toolbarEl.querySelector("#download");
   }
 
   initContext() {
@@ -59,6 +75,15 @@ class DrawingBoard {
     );
     this.undoEl.addEventListener("click", this.onClickUndo.bind(this));
     this.clearEl.addEventListener("click", this.onClickClear.bind(this));
+    this.downloadLinkEl.addEventListener(
+      "click",
+      this.onClickDownload.bind(this)
+    );
+  }
+
+  onClickDownload() {
+    this.downloadLinkEl.href = this.canvasEl.toDataURL("image/jpeg", 1);
+    this.downloadLinkEl.download = "example.jpeg";
   }
 
   onClickClear() {
